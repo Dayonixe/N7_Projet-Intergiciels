c0d0ecebf7202daa71b0bbce3889ca00764fa2ec
BlowOver <blowover38@gmail.com>
2022-05-05 15:40:39 +0200

    b

---
+++ TestReadBeforeTake.java
@@ -0,0 +1,80 @@
+package linda.test;
+
+import linda.Linda;
+import linda.Tuple;
+import linda.shm.CentralizedLinda;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(Parameterized.class)
+public class TestReadBeforeTake {
+    private static final Linda LINDA = new CentralizedLinda();
+
+    private static final Executor EXECUTOR = Executors.newCachedThreadPool();
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[5][0];
+    }
+
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
+        Tuple template = new Tuple(Integer.class, String.class);
+
+        Tuple theTuple = new Tuple(3, "Hello");
+
+        CompletableFuture<Tuple> taker = take(template);
+        CompletableFuture<Tuple> reader1 = read(template);
+        CompletableFuture<Tuple> reader2 = read(template);
+        CompletableFuture<Tuple> reader3 = read(template);
+
+        writeAfter(100, new Tuple(2, 3));
+        writeAfter(1000, theTuple);
+
+        CompletableFuture.allOf(taker, reader1, reader2, reader3);
+
+        assertEquals(theTuple, taker.get());
+        assertEquals(theTuple, reader1.get());
+        assertEquals(theTuple, reader2.get());
+        assertEquals(theTuple, reader3.get());
+
+        Tuple tuple = LINDA.tryRead(template);
+        assertNull(tuple);
+    }
+
+    private CompletableFuture<Tuple> read(Tuple template) {
+        return CompletableFuture.supplyAsync(() -> {
+            Tuple tuple = LINDA.read(template);
+            System.out.println("Read tuple: "+tuple);
+            return tuple;
+        }, EXECUTOR);
+    }
+
+    private CompletableFuture<Tuple> take(Tuple template) {
+        return CompletableFuture.supplyAsync(() -> {
+            Tuple tuple = LINDA.take(template);
+            System.out.println("Took tuple: "+tuple);
+            return tuple;
+        }, EXECUTOR);
+    }
+
+    private void writeAfter(long ms, Tuple tuple) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(ms);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            LINDA.write(tuple);
+        }, EXECUTOR);
+    }
+}
